47a0f35043923e541c2bd8d30107d7304777fcb5
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-08-11 22:09:52 +0500

    Add files via upload

---
+++ условная конструкция.py
@@ -0,0 +1,16 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+else:
+    print(0)
+first = 42
+second = 69
+third = 42
+if first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+    
+
